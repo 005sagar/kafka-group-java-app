621509c7dcbfb8b29e7d54aacf87b7b3308268b3
Tiwari <S527999@nwmissouri.edu>
2020-09-25 13:28:16 -0500

    created maven project and updated readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # kafka-group-java-app
 
-Team members:
+### Team members:
+**Leader:**
    * Sagar Tiwari
- 
+ **Members:**
+   * JL
+   * SP
+   * SM
---
+++ src/main/java/edu/nwmissouri/jsss04/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.jsss04;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this is jsss04 group!" );
+    }
+}
